f7e20268ac93a732450dbfad07d0027147bc333d
Jai Singhal <jaisinghal2000@gmail.com>
2021-08-18 14:10:34 +0530

    This is navbar sticky

--- src/app/navbar-slide/navbar-slide.component.ts
+++ src/app/navbar-slide/navbar-slide.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar-slide',
   templateUrl: './navbar-slide.component.html',
-  styleUrls: ['./navbar-slide.component.css']
+  styleUrls: ['./navbar-slide.component.css'],
 })
 export class NavbarSlideComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onTabClick(evt) {
+    this.setLineTab(evt.target);
+    console.log('EVENT', evt);
   }
 
+  setLineTab(tab) {
+    let line = document.querySelector<HTMLSelectElement>(' .tabs2 > .line');
+    line.style.left = tab.offsetLeft + 'px';
+    line.style.width = tab.clientWidth + 'px';
+  }
 }
